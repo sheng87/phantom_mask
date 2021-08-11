670add31e8a8389150bfd512e128fd128ab73875
sheng87 <4004171022kyu@gmail.com>
2021-08-11 15:10:12 +0800

    add hirb gem && set drugstore and mask edit, update, destroy url

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'api/v1/drugstore#index' 
+  namespace :api do
+    namespace :v1, defaults: {format: 'json'} do
+      get '/drugstores', to: 'drugstore#index'
+      get '/drugstores/:id', to: 'drugstore#edit'
+      get '/masks/:id', to: 'masks#edit'
+      put '/drugstores/:id', to: 'drugstore#update'
+      put '/masks/:id', to: 'masks#update'
+      delete 'masks/:mask_name', to: 'masks#destroy'
+    end
+  end
 end
